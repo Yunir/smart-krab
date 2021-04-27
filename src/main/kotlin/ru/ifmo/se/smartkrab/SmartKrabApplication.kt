23fc91c69c1330e34584b635b0c2fc19a5c42e8f
Yunir Salimzyanov <yunir.me@gmail.com>
2021-04-28 00:43:25 +0300

    Implement stub for adding user

--- src/main/kotlin/ru/ifmo/se/smartkrab/SmartKrabApplication.kt
+++ src/main/kotlin/ru/ifmo/se/smartkrab/SmartKrabApplication.kt
@@ -2,10 +2,22 @@ package ru.ifmo.se.smartkrab
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class SmartKrabApplication
+@RestController
+class SmartKrabApplication {
+    @GetMapping("/add-user")
+    fun addUser(
+        @RequestParam(value = "name", defaultValue = "World") name: String
+    ): String {
+        return "Add user: $name"
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<SmartKrabApplication>(*args)
+    runApplication<SmartKrabApplication>(*args)
 }
+
