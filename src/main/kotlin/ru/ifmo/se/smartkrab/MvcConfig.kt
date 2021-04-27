5c24e703efdd55ec23d8f011330fae96b843824e
Yunir Salimzyanov <yunir.me@gmail.com>
2021-04-28 01:56:30 +0300

    Add other pages, described in UC document

--- src/main/kotlin/ru/ifmo/se/smartkrab/MvcConfig.kt
+++ src/main/kotlin/ru/ifmo/se/smartkrab/MvcConfig.kt
@@ -10,5 +10,12 @@ class MvcConfig: WebMvcConfigurer {
         registry.addViewController("/").setViewName("home")
         registry.addViewController("/add-user").setViewName("add-user")
         registry.addViewController("/sign-in").setViewName("sign-in")
+        registry.addViewController("/extra-coins").setViewName("extra-coins")
+        registry.addViewController("/daily-report").setViewName("daily-report")
+        registry.addViewController("/new-order").setViewName("new-order")
+        registry.addViewController("/cooking").setViewName("cooking")
+        registry.addViewController("/add-new-tool").setViewName("add-new-tool")
+        registry.addViewController("/tools-status").setViewName("tools-status")
+        registry.addViewController("/antiplankton").setViewName("antiplankton")
     }
 }
