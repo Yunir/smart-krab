818f4030f1a120440947f0552c29fdb99442bdab
Yunir Salimzyanov <yunir.me@gmail.com>
2021-04-28 13:08:30 +0300

    Implement form processing on extra-coins page

---
+++ src/main/kotlin/ru/ifmo/se/smartkrab/ExtraCoinsController.kt
@@ -0,0 +1,32 @@
+package ru.ifmo.se.smartkrab
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+
+import org.springframework.web.bind.annotation.PostMapping
+
+
+
+
+
+@Controller
+class ExtraCoinsController {
+    @GetMapping("/extra-coins")
+    fun extraCoinsForm(model: Model): String {
+        model.addAttribute("greeting", Greeting())
+        return "extra-coins"
+    }
+
+    @PostMapping("/extra-coins")
+    fun extraCoinsSubmit(@ModelAttribute greeting: Greeting, model: Model): String {
+        model.addAttribute("greeting", greeting)
+        return "extra-coins-submit"
+    }
+}
+
+class Greeting {
+    var id: Long = 0
+    var content: String? = null
+}
