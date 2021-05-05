933ad93c04851b421a2fbf4785df8cbc7e574548
Yunir Salimzyanov <yunir.me@gmail.com>
2021-05-05 17:22:23 +0300

    Add validation for extra-coins input-field

--- src/main/kotlin/ru/ifmo/se/smartkrab/ExtraCoinsController.kt
+++ src/main/kotlin/ru/ifmo/se/smartkrab/ExtraCoinsController.kt
@@ -2,12 +2,14 @@ package ru.ifmo.se.smartkrab
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 
 import org.springframework.web.bind.annotation.PostMapping
 import ru.ifmo.se.smartkrab.data.ExtraCoins
 import ru.ifmo.se.smartkrab.data.ExtraCoinsRepository
+import javax.validation.Valid
 
 
 @Controller
@@ -20,7 +22,11 @@ class ExtraCoinsController(val ecRepo: ExtraCoinsRepository) {
     }
 
     @PostMapping("/extra-coins")
-    fun extraCoinsSubmit(@ModelAttribute extraCoins: ExtraCoins, model: Model): String {
+    fun extraCoinsSubmit(@ModelAttribute @Valid extraCoins: ExtraCoins, bindingResult: BindingResult, model: Model): String {
+        if (bindingResult.hasErrors()) {
+            return "extra-coins"
+        }
+
         model.addAttribute("extraCoins", extraCoins)
 
         ecRepo.save(extraCoins)
--- src/main/kotlin/ru/ifmo/se/smartkrab/data/ExtraCoins.kt
+++ src/main/kotlin/ru/ifmo/se/smartkrab/data/ExtraCoins.kt
@@ -5,11 +5,15 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.validation.constraints.*
 
 
 @Entity
 class ExtraCoins(
-    var value: Int = 0,
+    @field:NotNull(message = "Write value between 1 and 5000")
+    @field:Min(1, message = "value should be greater than 1")
+    @field:Max(5000, message = "value should be less than 5000")
+    var value: Int? = 0,
     var addedAt: LocalDateTime = LocalDateTime.now(),
     @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long? = null
 ) {
