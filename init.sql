4126dd98e941cf18d0d802517823daba94b5e3d0
Yunir Salimzyanov <yunir.me@gmail.com>
2021-10-27 02:43:21 +0300

    Make code more production-ready

--- init.sql
+++ init.sql
@@ -25,3 +25,17 @@ id bigserial not null primary key,
 name varchar,
 status integer
 );
+
+INSERT into extra_coins (value, added_at) values (5, current_timestamp);
+INSERT into extra_coins (value, added_at) values (1, current_timestamp);
+INSERT into extra_coins (value, added_at) values (7, current_timestamp);
+INSERT into extra_coins (value, added_at) values (2, current_timestamp);
+
+insert into order_info (position, value, status, added_at) values ('Kraby Pattie', 5, 'registered', current_timestamp);
+
+insert into krab_user (login, password, enabled, role)
+VALUES ('krusty', '$2a$10$x8pNO8z3H584w4T9K4.g8eOM5vHn5dhPUbRdW3xpl45XWR4ejYD5S', true, 'ROLE_OWNER'); -- krab
+insert into krab_user (login, password, enabled, role)
+VALUES ('squid', '$2a$10$irUhcMVzLeIME.z9RGIfSO1.3CpbgQPuGANLjb7Wd0Cp0k/5fRoge', true, 'ROLE_CASHIER'); -- test
+insert into krab_user (login, password, enabled, role)
+VALUES ('bob', '$2a$10$uj0Xz2wG10k1NuKN6Z6Xs.ZFaoDuNrX6tFSg5Bs6q/jbOeIf3uLju', true, 'ROLE_CHEF'); -- chef
--- src/main/kotlin/ru/ifmo/se/smartkrab/SmartKrabApplication.kt
+++ src/main/kotlin/ru/ifmo/se/smartkrab/SmartKrabApplication.kt
@@ -5,86 +5,68 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.password.PasswordEncoder
-import ru.ifmo.se.smartkrab.data.ExtraCoins
-import ru.ifmo.se.smartkrab.data.OrderInfo
-import ru.ifmo.se.smartkrab.data.Role
-import ru.ifmo.se.smartkrab.data.User
 import ru.ifmo.se.smartkrab.repository.ExtraCoinsRepository
 import ru.ifmo.se.smartkrab.repository.OrderRepository
 import ru.ifmo.se.smartkrab.repository.UserRepository
 
 
+fun main(args: Array<String>) {
+    runApplication<SmartKrabApplication>(*args)
+}
+
 @SpringBootApplication
 class SmartKrabApplication {
-/*
-    @Bean
-    fun commandLineRunner(ctx: ApplicationContext): CommandLineRunner? {
-        return CommandLineRunner { args: Array<String?>? ->
-            println("Let's inspect the beans provided by Spring Boot:")
-            val beanNames: Array<String> = ctx.beanDefinitionNames
-            Arrays.sort(beanNames)
-            for (beanName in beanNames) {
-                println(beanName)
-            }
-        }
-    }
-*/
 
     @Bean
     fun someExtraCoins(ecRepo: ExtraCoinsRepository): CommandLineRunner {
-        return CommandLineRunner { args: Array<String?>? ->
+        return CommandLineRunner {
             // save some coins
-            ecRepo.save(ExtraCoins(5))
-            ecRepo.save(ExtraCoins(1))
-            ecRepo.save(ExtraCoins(7))
-            ecRepo.save(ExtraCoins(2))
+//            ecRepo.save(ExtraCoins(5))
+//            ecRepo.save(ExtraCoins(1))
+//            ecRepo.save(ExtraCoins(7))
+//            ecRepo.save(ExtraCoins(2))
 
-            // fetch coins
+            // fetch coins from database
             println("ExtraCoins found with findAll():")
             println("-------------------------------")
-            for (extraCoins in ecRepo.findAll()) {
-                println(extraCoins.toString())
-            }
+            println(ecRepo.findAll().joinToString("\n"))
             println("")
         }
     }
 
     @Bean
     fun someOrders(oRepo: OrderRepository): CommandLineRunner {
-        return CommandLineRunner { args: Array<String?>? ->
+        return CommandLineRunner {
             // save some orders
-            oRepo.save(OrderInfo(value = 5))
+            // oRepo.save(OrderInfo(value = 5))
 
             println("Orders found with findAll():")
             println("-------------------------------")
-            for (extraCoins in oRepo.findAll()) {
-                println(extraCoins.toString())
-            }
+            println(oRepo.findAll().joinToString("\n"))
             println("")
         }
     }
 
     @Bean
     fun ownerUser(uRepo: UserRepository, passwordEncoder: PasswordEncoder): CommandLineRunner {
-        return CommandLineRunner { args: Array<String?>? ->
-            passwordEncoder.encode("krab")?.let { User(login = "krusty", password = it, role = Role.ROLE_OWNER, enabled = true) }?.let { uRepo.save(it) }
-
-            passwordEncoder.encode("test")?.let { User(login = "test", password = it, role = Role.ROLE_CASHIER, enabled = true) }?.let { uRepo.save(it) }
-
-            passwordEncoder.encode("chef")?.let { User(login = "chef", password = it, role = Role.ROLE_CHEF, enabled = true) }?.let { uRepo.save(it) }
+        return CommandLineRunner {
+//            passwordEncoder.encode("krab")
+//                ?.let { User(login = "krusty", password = it, role = Role.ROLE_OWNER, enabled = true) }
+//                ?.let { uRepo.save(it) }
+//
+//            passwordEncoder.encode("test")
+//                ?.let { User(login = "test", password = it, role = Role.ROLE_CASHIER, enabled = true) }
+//                ?.let { uRepo.save(it) }
+//
+//            passwordEncoder.encode("chef")
+//                ?.let { User(login = "chef", password = it, role = Role.ROLE_CHEF, enabled = true) }
+//                ?.let { uRepo.save(it) }
 
             println("Users found with findAll():")
             println("-------------------------------")
-            for (user in uRepo.findAll()) {
-                println(user)
-            }
+            println(uRepo.findAll().joinToString("\n"))
             println("")
         }
     }
-
-}
-
-fun main(args: Array<String>) {
-    runApplication<SmartKrabApplication>(*args)
 }
 
