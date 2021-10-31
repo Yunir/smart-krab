aec0bd39a64dfe974b2b2f2f34073cd941f89212
Yunir Salimzyanov <yunir.me@gmail.com>
2021-11-01 01:17:56 +0300

    Separate tests to easily find which test is failing in future

--- src/test/kotlin/ru/ifmo/se/smartkrab/access/AnonymousAccessTest.kt
+++ src/test/kotlin/ru/ifmo/se/smartkrab/access/AnonymousAccessTest.kt
@@ -2,6 +2,8 @@ package ru.ifmo.se.smartkrab.access
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -22,24 +24,20 @@ class AnonymousAccessTest(@Autowired val template: TestRestTemplate) {
         assertThat(entity.body).contains("Sign-in to your SmartKrab")
     }
 
-    @Test
-    fun redirectOtherPagesToSignInPage() {
-        val redirectPages = listOf(
-            "new-user",
-            "extra-coins",
-            "daily-report",
-            "new-order",
-            "cooking",
-            "new-tool",
-            "delete-tool",
-            "antiplankton",
-            "access-denied"
-        )
-
-        redirectPages.forEach {
-            assertThat(
-                template.getForEntity<String>("/$it").body
-            ).contains("Sign-in to your SmartKrab")
-        }
+    @ParameterizedTest(name = "{index}: redirect {0} to sign-in page")
+    @ValueSource(strings = arrayOf(
+        "new-user",
+        "extra-coins",
+        "daily-report",
+        "new-order",
+        "cooking",
+        "new-tool",
+        "delete-tool",
+        "antiplankton",
+        "access-denied"
+    ))
+    fun redirectOtherPagesToSignInPage(page: String) {
+            assertThat(template.getForEntity<String>("/$page").body)
+                .contains("Sign-in to your SmartKrab")
     }
 }
