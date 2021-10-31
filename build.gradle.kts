c822a3da614307dc7fdcc62ead43a500ee60d8f5
Yunir Salimzyanov <yunir.me@gmail.com>
2021-10-31 19:48:31 +0300

    Add 3 tests to check access for anonymous user

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,51 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.5"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.32"
-	kotlin("plugin.spring") version "1.4.32"
-	kotlin("plugin.allopen") version "1.4.32"
-	kotlin("plugin.jpa") version "1.4.32"
+    id("org.springframework.boot") version "2.4.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.4.32"
+    kotlin("plugin.spring") version "1.4.32"
+    kotlin("plugin.allopen") version "1.4.32"
+    kotlin("plugin.jpa") version "1.4.32"
 }
 
 group = "ru.ifmo.se"
-version = "0.0.1-SNAPSHOT"
+version = "0.1"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-devtools")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-devtools")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	runtimeOnly("com.h2database:h2")
-	implementation("org.postgresql:postgresql:42.2.5")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
-	testImplementation ("org.mockito.kotlin:mockito-kotlin:3.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.postgresql:postgresql:42.2.5")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
 }
 
 allOpen {
-	annotation("javax.persistence.Entity")
-	annotation("javax.persistence.Embeddable")
-	annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
---
+++ src/test/kotlin/ru/ifmo/se/smartkrab/AnonymousAccessTest.kt
@@ -0,0 +1,45 @@
+package ru.ifmo.se.smartkrab
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AnonymousAccessTest(@Autowired val template: TestRestTemplate) {
+
+    @Test
+    fun accessToHomePage() {
+        val entity = template.getForEntity<String>("/")
+        assertThat(entity.body).contains("Welcome to Smart Krab")
+    }
+
+    @Test
+    fun accessToSignInPage() {
+        val entity = template.getForEntity<String>("/sign-in")
+        assertThat(entity.body).contains("Sign-in to your SmartKrab")
+    }
+
+    @Test
+    fun redirectOtherPagesToSignInPage() {
+        val redirectPages = listOf(
+            "new-user",
+            "extra-coins",
+            "daily-report",
+            "new-order",
+            "cooking",
+            "new-tool",
+            "delete-tool",
+            "antiplankton",
+            "access-denied"
+        )
+
+        redirectPages.forEach {
+            assertThat(
+                template.getForEntity<String>("/$it").body
+            ).contains("Sign-in to your SmartKrab")
+        }
+    }
+}
